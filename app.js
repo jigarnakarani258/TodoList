f08ff1e0253666dc1ce3efe610eb538c046baec9
jigarnakarani258 <nakaranijigar258@gmail.com>
2023-10-01 16:04:18 +0530

    Add Custom Error Class and global error controller

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express();
 
+const {globalErrController} = require('./controllers/errorController')
+const {AppError} =require('./utility/appError')
+
+
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
@@ -17,5 +21,17 @@ app.use( (req , res , next) =>{
 
 
 
+//here app.all use for all method(get,post,put,delete)
+//Unhandled Routes Handling 
+app.all('*',(req,res,next)=>{
+    let err = {
+        name : 'customPathError',
+        message : `Can not find route ${req.originalUrl} on this server, Please check API route.`
+    }
+    return next(new AppError(err, 404));
+})
+
+//Global error Middleware // Ex->{ next(err)} 
+app.use(globalErrController)
 
 module.exports = app;
